9c7e3d1adf2e4775ac4e6f089f7926942ac7d35b
AJ ONeal <awesome@coolaj86.com>
2016-09-22 16:18:35 -0600

    demo works

--- client.js
+++ client.js
@@ -6,26 +6,34 @@ var sni = require('sni');
 // TODO ask oauth3.org where to connect
 // TODO reconnect on disconnect
 
-function pack(address, data) {
-  var version = 1;
-  var header = /*servername + ',' +*/address.family + ',' + address.address + ',' + address.port + ',' + data.byteLength;
-  var meta = [ 255 - version, header.length ];
-  var buf = Buffer.alloc(meta.length + header.length + data.byteLength);
-
-  buf.write(meta[0], 0);
-  buf.write(meta[1], 1);
-  buf.write(header, 2);
-  buf.write(data, 2 + header.length);
-
-  return buf;
-}
-
 // Assumption: will not get next tcp packet unless previous packet succeeded
 //var services = { 'ssh': 22, 'http': 80, 'https': 443 };
 var services = { 'ssh': 22, 'http': 4080, 'https': 8443 };
-var tunneler = net.connect({ port: 5443 , host: 'pokemap.hellabit.com' }, function () {
-  var token = jwt.sign({ name: 'pokemap.hellabit.com' }, 'shhhhh');
-  var clients = {};
+var hostname = 'aj.daplie.me'; // 'pokemap.hellabit.com'
+
+function addrToId(address) {
+  return address.family + ',' + address.address + ',' + address.port;
+}
+
+/*
+function socketToAddr(socket) {
+  return { family: socket.remoteFamily, address: socket.remoteAddress, port: socket.remotePort };
+}
+
+function socketToId(socket) {
+  return addrToId(socketToAddr(socket));
+}
+*/
+
+var tunneler = net.connect({ port: 5443 , host: hostname }, function () {
+  var token = jwt.sign({ name: hostname }, 'shhhhh');
+  var localclients = {};
+
+  setInterval(function () {
+    console.log('');
+    console.log('localclients.length:', Object.keys(localclients).length);
+    console.log('');
+  }, 5000);
 
   tunneler.write(token);
 
@@ -34,15 +42,30 @@ var tunneler = net.connect({ port: 5443 , host: 'pokemap.hellabit.com' }, functi
   // a place to store data
   // file management
   // Synergy Teamwork Paradigm = Jabberwocky
-  var machine = require('./machine.js').create();
-  machine.onMessage = function (opts) {
-    var id = opts.family + ',' + opts.address + ',' + opts.port;
+  var pack = require('tunnel-packer').pack;
+
+  function onMessage(opts) {
+    var id = addrToId(opts);
     var service = 'https';
     var port = services[service];
-    var client;
+    var lclient;
 
-    if (clients[id]) {
-      clients[id].write(opts.data);
+    if (opts.data.byteLength < 20) {
+      if ('|__ERROR__|' === opts.data.toString('utf8')
+        || '|__END__|' === opts.data.toString('utf8')) {
+
+        console.log("end '" + opts.address + "'");
+        if (localclients[id]) {
+          localclients[id].end();
+          delete localclients[id];
+        }
+        return;
+      }
+    }
+
+    if (localclients[id]) {
+      console.log("received data from '" + opts.address + "'", opts.data.byteLength);
+      localclients[id].write(opts.data);
       return;
     }
 
@@ -50,30 +73,36 @@ var tunneler = net.connect({ port: 5443 , host: 'pokemap.hellabit.com' }, functi
 
     if (!servername) {
       console.warn("no servername found for '" + id + "'");
-      tunneler.write(pack(opts, '|__ERROR__|'));
+      tunneler.write(pack(opts, Buffer.from('|__ERROR__|')));
       return;
     }
 
     console.log("servername: '" + servername + "'");
 
-    clients = clients[id] = net.createConnect({ port: port, host: servername }, function () {
-      client.on('data', function (chunk) {
-        console.error('client Data');
+    lclient = localclients[id] = net.createConnection({ port: port, host: '127.0.0.1' }, function () {
+
+      lclient.on('data', function (chunk) {
+        console.log("client '" + opts.address + "' sent ", chunk.byteLength, "bytes");
         tunneler.write(pack(opts, chunk));
       });
-      client.on('error', function (err) {
+      lclient.on('error', function (err) {
         console.error('client Error');
         console.error(err);
-        tunneler.write(pack(opts, '|__ERROR__|'));
+        delete localclients[id];
+        tunneler.write(pack(opts, Buffer.from('|__ERROR__|')));
       });
-      client.on('end', function () {
-        console.error('client End');
-        tunneler.write(pack(opts, '|__END__|'));
+      lclient.on('end', function () {
+        console.log('client End');
+        delete localclients[id];
+        tunneler.write(pack(opts, Buffer.from('|__END__|')));
       });
 
-      client.write(opts.data);
+      console.log('received data', opts.data.byteLength);
+      lclient.write(opts.data);
     });
-  };
+  }
+
+  var machine = require('tunnel-packer').create({ onMessage: onMessage });
 
   tunneler.on('data', machine.fns.addChunk);
 
