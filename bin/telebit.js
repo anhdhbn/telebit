f2f85cfa188374f88d8f07a73d577f5de1fcb0e7
AJ ONeal <coolaj86@gmail.com>
2018-09-11 02:02:15 -0600

    add sclient with inverse ssh proxying

--- bin/telebit.js
+++ bin/telebit.js
@@ -5,6 +5,13 @@
 // node telebit daemon arg1 arg2
 if ('daemon' === process.argv[2]) {
   require('./telebitd.js');
+} else if ('sclient' === process.argv[2]) {
+  process.argv.splice(1,1);
+  require('sclient/bin/sclient.js');
+} else if ('ssh' === process.argv[2] && /[\w-]+\.[a-z]{2,}/i.test(process.argv[3])) {
+  process.argv.splice(1,1,'sclient');
+  process.argv.splice(2,1,'--ssh');
+  require('sclient/bin/sclient.js');
 } else {
   require('./telebit-remote.js');
 }
